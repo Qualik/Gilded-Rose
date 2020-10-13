215419601b465b8725b988d7daa65e42a76b7ffb
Dwayne Rainford <qualik2ko@gmail.com>
2020-10-13 19:36:11 +0100

    first commit

---
+++ js/spec/gilded_rose_spec.js
@@ -0,0 +1,65 @@
+describe("Gilded Rose", function() {
+
+  it("should foo", function() {
+    items = [ new Item("foo", 0, 0) ];
+    update_quality();
+    expect(items[0].name).toEqual("foo");
+  });
+
+  
+describe("sellIn value", () => {
+  it("Items have a sellIn value which shows the number of days left to sell the item", () => {
+    var daysLeft = [0];
+  
+    expect(daysLeft[0].items).toEqual("sellIns value");
+  });
+
+  
+
+  it("Items have a Quality value which shows how valuable the item is", () => {
+    expect().toEqual("");
+  });
+
+  it("End of each day system lowers both values for every item", () => {
+    expect().toEqual("");
+  });
+
+  it("Quality degrades twice as fast when sell by date has passed", () => {
+    expect().toEqual("");
+  });
+
+  it("Quality of an item is never negative", () => {
+    expect().toEqual("");
+  });
+
+  it("Aged Brie increases in Quality the older it gets", () => {
+    expect().toEqual("");
+  });
+
+  it("Quality if an item is never greater than 50", () => {
+    expect().toEqual("");
+  });
+
+  it("Sulfuras is a legendary item and never has to be sold or decreases in Quality", () => {
+    items = 
+    expect(items).toEqual("");
+  });
+
+  it("Backstge passes increase in Quality as its sellIn value approaches", () => {
+    expect().toEqual("");
+  });
+
+  it("Quality increases by 2 when there 10 days or less", () => {
+    expect().toEqual("");
+  });
+
+  it("Quality increases by 3 when there 5 days or less", () => {
+    expect().toEqual("");
+  });
+
+  it("Quality becomes 0 after the concert", () => {
+    expect().toEqual("");
+  });
+
+  });
+});
